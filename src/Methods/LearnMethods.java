9c10f4da231a20c5bacaa90e05186edcbc5cc575
Amresh <138843165+ammunirala@users.noreply.github.com>
2026-01-27 02:09:31 +0530

    Basics of java

--- src/Methods/LearnMethods.java
+++ src/Methods/LearnMethods.java
@@ -2,23 +2,29 @@ package Methods;
 
 public class LearnMethods {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        greet();
-        System.out.println(3);
-        System.out.println(4);
+//        System.out.println(1);
+//        System.out.println(2);
+//        greet();
+//        System.out.println(3);
+//        System.out.println(4);
+        average (4,6);
+        System.out.println("Hello");
+        average(2,7);
 
     }
 
-   static void greet(){
-       System.out.println(5);
-        System.out.println("Hello World");
-       System.out.println(6);
-    }
-
-//    public void average(int a, int b){
-//
-//
-//
+//   static void greet(){
+//       System.out.println(5);
+//        System.out.println("Hello World");
+//       System.out.println(6);
 //    }
+
+
+    public static void average(int a, int b){
+        int avg = (a+b)/2;
+        System.out.println("the average is " +avg);
+
+
+
+    }
 }
---
+++ src/Methods/MathClass01.java
@@ -0,0 +1,19 @@
+package Methods;
+
+public class MathClass01 {
+    public static void main(String[] args) {
+        int a = -15;
+        int b = 20;
+        System.out.println(Math.abs(a));
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.pow(a, b));
+        System.out.println(Math.sqrt(49));
+        System.out.println(Math.pow(3, 2));
+
+
+
+
+    }
+}
