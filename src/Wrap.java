d90650f6c1938bde1a88b2b86d1d9719739007b5
Amresh <138843165+ammunirala@users.noreply.github.com>
2026-01-26 23:40:43 +0530

    Arrays

--- src/Wrap.java
+++
@@ -1,6 +0,0 @@
-class Wrap {
-    public static void main(String[] args){
-        int x = Integer.parseInt("123");
-        Integer.valueOf("123");
-    }
-}
---
+++ src/multi_dimensional/first.java
@@ -0,0 +1,33 @@
+package multi_dimensional;
+
+public class first {
+    public static void main(String[] args) {
+        int marks[][] = {
+                {12,98,34},
+                {12,67,87},
+                {12,98,23},
+                {14,67,36}
+        };
+        System.out.println(marks[3][2]);
+
+//        marks[0][0] = 12;
+//        marks[0][1] = 98;
+//        marks[0][2] = 34;
+//
+//        marks[1][0] = 12;
+//        marks[1][1] = 67;
+//        marks[1][2] = 87;
+//
+//        marks[2][0] = 12;
+//        marks[2][1] = 98;
+//        marks[2][2] = 34;
+//
+//        marks[3][0] = 12;
+//        marks[3][1] = 67;
+//        marks[3][2] = 87;
+
+
+
+    }
+
+}
